e5e88de73f5cb19a52cb461385e47fb47460aa46
logan <logan.christopher@comcast.net>
2023-11-02 00:10:20 -0500

    - added procloc - added pwsh profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -33,3 +33,14 @@ function run { dotnet run $args }
 Set-Alias -Name dr -Value run
 function build { dotnet build $args }
 Set-Alias -Name db -Value build 
+
+function procloc { 
+    if ((Test-Path -Path $FileOrFolderPath) -eq $false) {
+        Write-Warning "File or directory does not exist."
+    } else {
+        $LockingProcess = CMD /C "openfiles /query /fo table | find /I ""$FileOrFolderPath"""
+        Write-Host $LockingProcess
+    } 
+}
+Set-Alias -Name procloc -Value procloc
+Set-Alias -Name pc -Value procloc
